9e1ac3d941716924b6b42dd3b925288c5c77d182
crosstyan <crosstyan@outlook.com>
2025-04-24 12:13:50 +0800

    Add support for GLB files in LFS tracking, enhance find_cute_box_with_image.ipynb with new functions for 3D coordinate extraction, and introduce interactive_example.py for marker processing workflow.

---
+++ interactive_example.py
@@ -0,0 +1,18 @@
+# %%
+import numpy as np
+
+
+# %%
+
+# %% [markdown]
+# # Extract the 3D coordinates of the ArUco markers from the image
+#
+# 1. Load the image
+# 2. Detect the ArUco markers
+# 3. Get the 3D coordinates of the markers
+# 4. Save the 3D coordinates to a file
+
+
+# %%
+
+# %%
